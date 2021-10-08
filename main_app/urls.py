142b757459ca775da393033b17311169702ab03f
Mehari Abraha <mehariogbe@yahoo.com>
2021-10-07 22:52:51 -0500

    add create and detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
    path('', views.Home.as_view(), name="home"),
 #    Cars  urls
    path('cars/', views.CarsList.as_view(), name="cars_list"),
-
+   path('cars/new', views.CarsCreate.as_view(), name="cars_create"),
+   path('cars/<int:pk>/', views.CarsDetail.as_view(), name="cars_detail"),
 
    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic.base import TemplateView
 from django.views import View
 from django.http import HttpResponse
 from .models import Cars
+from django.views.generic.edit import CreateView, DeleteView
 
 # Create your views here.
 class Home(TemplateView):
@@ -14,5 +15,22 @@ class CarsList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["carss"] = Cars.objects.all()
+        # query for search button
+        make =self.request.GET.get("make")
+        if make != None:
+            context["carss"] = Cars.objects.filter(make__icontains=make)
+        else:    
+            context["carss"] = Cars.objects.all()
+            # default header
+            context["header"] = "Cars Make"
         return context
+
+class CarsCreate(CreateView):
+    model = Cars
+    fields = ['make', 'img', 'country']
+    template_name = "cars_create.html"
+    success_url = "/cars/"
+
+class CarsDetail(DeleteView):
+    model = Cars
+    template_name = "cars_detail.html"
