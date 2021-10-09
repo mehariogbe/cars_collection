8023e98a4093283c154b2d0a8f0a6b6199675304
Mehari Abraha <mehariogbe@yahoo.com>
2021-10-09 18:14:04 -0500

    add gitignore

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # import datetime
 # from partial_date import PartialDate
 # from django_countries.fields import CountryField
