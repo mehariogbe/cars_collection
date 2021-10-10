e9d22828c973c6ade3c57f8c2b5f8f5bdd2bf430
Mehari Abraha <mehariogbe@yahoo.com>
2021-10-09 19:38:47 -0500

    finish with auth and sign up template

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
    path('', views.Home.as_view(), name="home"),
    
-#    Cars  urls
+#  Cars  urls
    path('cars/', views.CarsList.as_view(), name="cars_list"),
    path('cars/new', views.CarsCreate.as_view(), name="cars_create"),
    path('cars/<int:pk>/', views.CarsDetail.as_view(), name="cars_detail"),
@@ -17,4 +17,6 @@ urlpatterns = [
 #  Favorite url
    path('cars/favorite', views.Fav.as_view(), name="favorite"),
    path('favorites/<int:pk>/models/<int:model_pk>/', views.FavoriteModelAssoc.as_view(), name="favorite_model_assoc"),
+#  user signup url 
+   path('accounts/signup/', views.Signup.as_view(), name="signup"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,10 @@ from .models import Cars, Model, Favorite
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from django.urls import reverse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
@@ -21,6 +25,7 @@ class Home(TemplateView):
 
 
 # Views for Cars
+@method_decorator(login_required, name='dispatch')
 class CarsList(TemplateView):
     template_name = "cars_list.html"
 
@@ -103,4 +108,23 @@ class FavoriteModelAssoc(View):
             Favorite.objects.get(pk=pk).models.remove(model_pk)
         if assoc == "add":
             Favorite.objects.get(pk=pk).models.add(model_pk)
-        return redirect('favorite')        
+        return redirect('favorite')  
+
+        # Signup auth view
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("cars_list")
+        else:
+            context ={"form": form}
+            return render(request, "registration/signup.html", context)
+
+
